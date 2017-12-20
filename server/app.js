553916407615ccb432acd0c608adac3dc0640bfe
anthonytxie <anthony.xie@vidyard.com>
2017-12-20 18:23:21 -0500

    basic static setup

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const publicPath = __dirname + '/../public'
+
+const app = express();
+app.use(express.static(publicPath));
+
+//Middleware
+
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+const config = require('./../config/config');
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
