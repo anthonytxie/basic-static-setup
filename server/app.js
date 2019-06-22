0d754c053f0acc078707e5c2aadf5950f8d72d88
Anthony <anthonytxie@gmail.com>
2019-06-22 19:07:05 -0400

    back to basic static setup template

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const publicPath = __dirname + "/../public";
-const easyFilePath = __dirname + "/../public/easy.html";
+
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const smtpTransport = require("nodemailer-smtp-transport");
@@ -11,42 +11,6 @@ app.use(express.static(publicPath));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/easy", (req, res) => {
-  res.sendFile(path.resolve(easyFilePath));
-});
-
-app.post("/contact", (req, res) => {
-  console.log(req.body);
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport(
-    smtpTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.email,
-        pass: process.env.password
-      }
-    })
-  );
-  // setup email data with unicode symbols
-  let mailOptions = {
-    from: req.body.email, // sender address
-    to: ["anthonytxie@gmail.com"], // list of receivers
-    subject: "Contact Us Form", // Subject line
-    text: "sent by: " + req.body.email
-
-    // plain text body
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("Message %s sent: %s", info.messageId, info.response);
-  });
-  res.redirect("/");
-  // res.redirect('/');
-});
 //Middleware
 
 module.exports = app;
